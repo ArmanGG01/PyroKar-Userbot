8073c6cfec21e4d617cf4b9ca89a03e0de5f8bae
Hadi <108177985+ThePowerOfKang@users.noreply.github.com>
2022-07-02 15:09:47 +0700

    Initial commit

---
+++ PyroKar/__main__.py
@@ -0,0 +1,49 @@
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from pyrogram import idle
+from uvloop import install
+
+from config import BOT_VER, CMD_HANDLER
+from PyroKar import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bots
+from PyroKar.helpers.misc import git, heroku
+
+MSG_ON = """
+🔥 **PyroKar-Userbot Berhasil Di Aktifkan**
+━━
+➠ **Userbot Version -** `{}`
+➠ **Ketik** `{}alive` **untuk Mengecheck Bot**
+━━
+"""
+
+
+async def main():
+    for bot in bots:
+        try:
+            await bot.start()
+            bot.me = await bot.get_me()
+            await bot.join_chat("Lunatic0de")
+            await bot.join_chat("SharingUserbot")
+            await bot.join_chat("hdiiofficial") 
+            # Tambahin sendiri klo mau gaush di hapus stres
+            await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER))
+        except Exception as a:
+            LOGGER("main").warning(a)
+    await idle()
+    await aiosession.close()
+
+
+if __name__ == "__main__":
+    LOGGER("PyroKar").info("Starting PyroKar-UserBot")
+    LOGGER("PyroKar").info(f"Total Clients = {len(bots)} Users")
+    install()
+    git()
+    heroku()
+    LOGGER("PyroKar").info(f"PyroKar-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    LOOP.run_until_complete(main())
