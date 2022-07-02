19335fd0afc622c8e4802fc8e7a8467278167074
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-07-02 23:42:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -16,14 +16,14 @@ from dotenv import load_dotenv
 load_dotenv("config.env")
 
 
-ALIVE_EMOJI = getenv("ALIVE_EMOJI", "⚡️")
+ALIVE_EMOJI = getenv("ALIVE_EMOJI", "👑")
 ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/9dc4e335feaaf6a214818.jpg")
 ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, I am alive.")
 API_HASH = getenv("API_HASH")
 API_ID = int(getenv("API_ID", ""))
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
-    BLACKLIST_CHAT = [-1001473548283]
+    BLACKLIST_CHAT = [-1001473548283, -1001302879778]
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 BOT_VER = "1.1.1@main"
 BRANCH = "main"
@@ -36,7 +36,7 @@ GIT_TOKEN = getenv(
         "utf-8"
     ),
 )
-GROUP = getenv("GROUP", "starcrushh")
+GROUP = getenv("GROUP", "obrolansuar")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
