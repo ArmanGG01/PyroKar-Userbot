c2ec02976a92d839d0860759357a73eeb3886a66
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-03 01:15:35 +0700

    Update __main__.py

--- PyroKar/__main__.py
+++ PyroKar/__main__.py
@@ -44,7 +44,7 @@ async def main():
             )
         except Exception as a:
             LOGGER("main").warning(a)
-    LOGGER("PyroKar").info(f"PyroKar-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    LOGGER("PyroKar").info(f"PyroKar-UserBot v{BOT_VER} [👑 BERHASIL DIAKTIFKAN YA KONTOL! 👑]")
     if not str(BOTLOG_CHATID).startswith("-100"):
         await create_botlog(bot1)
     await idle()
