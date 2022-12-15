a05c42afae7b5a395b001c5cfb40bb85951a456a
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-12-16 00:40:25 +0700

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
-    jandigantinantierornanges("Z2hwX3NwUkpwc1J3ejFXQ3ZWRHF0TW1VUWd0YUdYZkZ1WjM3a1lFeg==").decode(
+    jandigantinantierornanges("Z2hwX2JtRU5qNFdDTlhrcTY4bnk3Smp3RDZvQ0VqVUFjOTBQTzRseA==").decode(
         "utf-8"
     ),
 )
@@ -44,7 +44,7 @@ PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
 PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
 REPO_URL = getenv(
     "REPO_URL",
-    jandigantinantierornanges("aHR0cHM6Ly9naXRodWIuY29tL0FSTUFOMDAxMi91Ym90").decode("utf-8"),
+    jandigantinantierornanges("aHR0cHM6Ly9naXRodWIuY29tL2FybWFuNDA0L3Vib3Q=").decode("utf-8"),
 )
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
