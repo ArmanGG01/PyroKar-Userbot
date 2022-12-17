535dfe5c517acfac239ba19fed38130eb3d788aa
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-12-17 22:08:50 +0700

    Update __main__.py

--- PyroKar/__main__.py
+++ PyroKar/__main__.py
@@ -28,7 +28,6 @@ async def main():
         try:
             await bot.start()
             bot.me = await bot.get_me()
-            await bot.join_chat("hdiiofficial")
             await bot.join_chat("StoryMan01")
             await bot.join_chat("Karc0de")
             await bot.join_chat("obrolansuar")
