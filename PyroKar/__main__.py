7ac39ab26537236adafaae3d6029984d06d32bb1
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-21 00:57:20 +0700

    Update __main__.py

--- PyroKar/__main__.py
+++ PyroKar/__main__.py
@@ -7,29 +7,37 @@
 #
 # t.me/SharingUserbot & t.me/Lunatic0de
 
+import importlib
+
 from pyrogram import idle
 from uvloop import install
 
 from config import BOT_VER, CMD_HANDLER
-from PyroKar import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
-from PyroKar.helpers.misc import create_botlog, git, heroku
+from PyroKar import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots, app, ids
+from PyroKar.helpers.misc import create_botlog, heroku
+from PyroKar.modules import ALL_MODULES
 
 MSG_ON = """
 💢 **PyroKar-Userbot Udah Aktif** 💢
 ╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
 ❍▹ **Userbot Version -** `{}`
-❍▹ **Ketik** `{}alive` **untuk Mengecheck Bot**
+❍▹ **Ketik** `{}kar` **untuk Mengecheck Bot**
 ╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
 """
 
 
 async def main():
+	await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module(f"PyroKar.modules.{all_module}")
     for bot in bots:
         try:
             await bot.start()
             bot.me = await bot.get_me()
+            await bot.join_chat("hdiiofficial")
             await bot.join_chat("StoryMan01")
-            await bot.join_chat("Karc0de")
+            await bot.join_chat("Pyr0kar")
+            await bot.join_chat("TestiAllPaymat")
             await bot.join_chat("obrolansuar")
             try:
                 await bot.send_message(
@@ -40,6 +48,7 @@ async def main():
             LOGGER("PyroKar").info(
                 f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
             )
+            ids.append(ex.id)
         except Exception as a:
             LOGGER("main").warning(a)
     LOGGER("PyroKar").info(f"PyroKar-UserBot v{BOT_VER} [👑 BERHASIL DIAKTIFKAN YA KONTOL! 👑]")
@@ -52,6 +61,5 @@ async def main():
 if __name__ == "__main__":
     LOGGER("PyroKar").info("Starting PyroKar-UserBot")
     install()
-    git()
     heroku()
     LOOP.run_until_complete(main())
