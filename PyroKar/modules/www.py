4b2b14297389b6e3d3b3c471bf50cbdd9f44ea58
ArmanGG01 <muhammadarrif2f@gmail.com>
2023-01-23 19:00:13 +0000

    ArmanGG01: auto fixes
    Signed-off-by: ArmanGG01 <muhammadarrif2f@gmail.com>

--- PyroKar/modules/www.py
+++ PyroKar/modules/www.py
@@ -14,10 +14,6 @@ import speedtest
 from pyrogram import Client, filters
 from pyrogram.raw import functions
 from pyrogram.types import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    InlineQueryResultArticle,
-    InputTextMessageContent,
     Message,
 )
 from config import CMD_HANDLER as cmd
