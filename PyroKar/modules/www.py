0491394fb5c7427613042f040735046d6bedcd6a
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-24 01:46:21 +0700

    Update www.py

--- PyroKar/modules/www.py
+++ PyroKar/modules/www.py
@@ -13,9 +13,7 @@ from datetime import datetime
 import speedtest
 from pyrogram import Client, filters
 from pyrogram.raw import functions
-from pyrogram.types import (
-    Message,
-)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent, Message
 
 from config import CMD_HANDLER as cmd
 from config import BOT_VER, BRANCH as brch
