326d704c02ee12c78c618563a0d7adf966ddb528
ArmanGG01 <muhammadarrif2f@gmail.com>
2023-01-23 18:47:45 +0000

    ArmanGG01: auto fixes
    Signed-off-by: ArmanGG01 <muhammadarrif2f@gmail.com>

--- PyroKar/modules/www.py
+++ PyroKar/modules/www.py
@@ -13,7 +13,7 @@ from datetime import datetime
 import speedtest
 from pyrogram import Client, filters
 from pyrogram.raw import functions
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent, Message
+from pyrogram.types import Message
 
 from config import CMD_HANDLER as cmd
 from config import BOT_VER, BRANCH as brch
