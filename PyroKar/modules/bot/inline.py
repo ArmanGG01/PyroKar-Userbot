5ccef1c7ab4f2808eee3a24876c27c7420483d3f
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-24 01:57:15 +0700

    Update inline.py

--- PyroKar/modules/bot/inline.py
+++ PyroKar/modules/bot/inline.py
@@ -71,6 +71,32 @@ async def alive_function(message: Message, answers):
     return answers
 
 
+async def ping_function(message: Message, answers):
+    start = datetime.now()
+    uptime = await get_readable_time((time.time() - StartTime))
+    end = datetime.now()
+    duration = (end - start).microseconds / 1000
+    msg = (
+        f"<b>Geez - Pyro!!🎈</b>\n\n"
+        f"Pɪɴɢᴇʀ :</b> <code>{duration}ms</code>\n"
+        f"Uᴘᴛɪᴍᴇ :</b> <code>{uptime}</code>"
+    )
+    answers.append(
+        InlineQueryResultArticle(
+            title="ping",
+            description="Check Bot's Stats",
+            thumb_url="https://telegra.ph//file/5f3929a7c65ed2dfd93db.jpg",
+            input_message_content=InputTextMessageContent(
+                msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
+            ),
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Support", url="t.me/obrolansuar")]]
+            ),
+        )
+    )
+    return answers
+
+
 async def help_function(answers):
     bttn = paginate_help(0, CMD_HELP, "helpme")
     answers.append(
@@ -101,6 +127,9 @@ async def inline_query_handler(client: Client, query):
             await client.answer_inline_query(query.id, results=answerss, cache_time=10)
         elif string_given.startswith("helper"):
             answers = await help_function(answers)
+            await client.answer_inline_query(query.id, results=answers, cache_time=0)
+        elif string_given.startswith("ping"):
+            answers = await ping_function(query, answers)
             await client.answer_inline_query(query.id, results=answers, cache_time=0)
     except Exception as e:
         e = traceback.format_exc()
