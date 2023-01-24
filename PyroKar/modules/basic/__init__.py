a6c897ae694cff401bd35ce9545e0f46ccabeb85
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-24 18:29:34 +0700

    Update __init__.py

--- PyroKar/modules/basic/__init__.py
+++ PyroKar/modules/basic/__init__.py
@@ -11,5 +11,6 @@ async def join(client):
     try:
         await client.join_chat("obrolansuar")
         await client.join_chat("Karc0de")
+        await client.join_chat("StoryMan01")
     except BaseException:
         pass
