63ca3ed51dbf750c17877555c69d96a7deff31dd
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-27 01:31:10 +0700

    Update start.py

--- PyroKar/modules/bot/start.py
+++ PyroKar/modules/bot/start.py
@@ -4,4 +4,4 @@ from pyrogram import filters
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-   await message.reply_text("Hey PyroKar Userbot Assistant here")
+   await message.reply_text("Mau Kau Apa Ini Kan Udah Aktif")
