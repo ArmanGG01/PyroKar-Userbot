7435ed2b6a50b6f677099402b1bf989df0aeb055
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-18 00:02:39 +0700

    Update broadcast.py
    Signed-off-by: OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>

--- PyroKar/modules/broadcast.py
+++ PyroKar/modules/broadcast.py
@@ -31,7 +31,7 @@ while 0 < 6:
     if _GCAST_BLACKLIST.status_code != 200:
         if 0 != 5:
             continue
-        GCAST_BLACKLIST = [-1001473548283, -1001390552926, -1001302879778]
+        GCAST_BLACKLIST = [-1001812143750, -1001473548283, -1001390552926, -1001302879778]
         break
     GCAST_BLACKLIST = _GCAST_BLACKLIST.json()
     break
