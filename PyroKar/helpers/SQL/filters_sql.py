6b119fae7861c79f6235df9f0fdf9c360083aa43
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-03 03:19:53 +0700

    update filters_sql.py

--- PyroKar/helpers/SQL/filters_sql.py
+++ PyroKar/helpers/SQL/filters_sql.py
@@ -1,18 +1,19 @@
-try:
-    from PyroKar.helpers.SQL import BASE, SESSION
-except ImportError:
-    raise AttributeError
+# Kata Gua Lu Anjing
+
+from . import BASE, SESSION
 from sqlalchemy import Column, Numeric, String, UnicodeText
 
 
 class Filters(BASE):
     __tablename__ = "filters"
+    user_id = Column(String(14), primary_key=True)
     chat_id = Column(String(14), primary_key=True)
     keyword = Column(UnicodeText, primary_key=True, nullable=False)
     reply = Column(UnicodeText)
     f_mesg_id = Column(Numeric)
 
-    def __init__(self, chat_id, keyword, reply, f_mesg_id):
+    def __init__(self, user_id, chat_id, keyword, reply, f_mesg_id):
+        self.user_id = str(user_id)
         self.chat_id = str(chat_id)
         self.keyword = keyword
         self.reply = reply
@@ -21,6 +22,7 @@ class Filters(BASE):
     def __eq__(self, other):
         return bool(
             isinstance(other, Filters)
+            and self.user_id == other.user_id
             and self.chat_id == other.chat_id
             and self.keyword == other.keyword
         )
@@ -29,42 +31,42 @@ class Filters(BASE):
 Filters.__table__.create(checkfirst=True)
 
 
-def get_filter(chat_id, keyword):
+def get_filter(user_id, chat_id, keyword):
     try:
-        return SESSION.query(Filters).get((str(chat_id), keyword))
+        return SESSION.query(Filters).get((str(user_id), chat_id, keyword))
     finally:
         SESSION.close()
 
 
-def get_filters(chat_id):
+def get_filters(user_id, chat_id):
     try:
-        return SESSION.query(Filters).filter(Filters.chat_id == str(chat_id)).all()
+        return SESSION.query(Filters).filter(Filters.user_id == str(user_id), Filters.chat_id == str(chat_id)).all()
     finally:
         SESSION.close()
 
 
-def add_filter(chat_id, keyword, reply, f_mesg_id):
-    to_check = get_filter(chat_id, keyword)
-    if not to_check:
-        adder = Filters(str(chat_id), keyword, reply, f_mesg_id)
-        SESSION.add(adder)
+def add_filter(user_id, chat_id, keyword, reply, f_mesg_id):
+    lu_babi = get_filter(user_id, chat_id, keyword)
+    if not lu_babi:
+        bangsat = Filters(str(user_id), chat_id, keyword, reply, f_mesg_id)
+        SESSION.add(bangsat)
         SESSION.commit()
         return True
     else:
-        rem = SESSION.query(Filters).get((str(chat_id), keyword))
-        SESSION.delete(rem)
+        onyet = SESSION.query(Filters).get((str(user_id), chat_id, keyword))
+        SESSION.delete(onyet)
         SESSION.commit()
-        adder = Filters(str(chat_id), keyword, reply, f_mesg_id)
-        SESSION.add(adder)
+        bangsat = Filters(str(user_id), chat_id, keyword, reply, f_mesg_id)
+        SESSION.add(bangsat)
         SESSION.commit()
         return False
 
 
-def remove_filter(chat_id, keyword):
-    to_check = get_filter(chat_id, keyword)
-    if not to_check:
+def remove_filter(user_id, chat_id, keyword):
+    tai = get_filter(user_id, chat_id, keyword)
+    if not tai:
         return False
     else:
-        SESSION.delete(to_check)
+        SESSION.delete(tai)
         SESSION.commit()
         return True
